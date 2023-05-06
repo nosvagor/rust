8ba73b481d9f7e5c0e1c5e743fd16d22c0cd0232
nosvagor <nosvagor@gmail.com>
2023-05-06 14:32:27 -0700

    redoing the deleted exercises

--- rustlings/exercises/enums/enums2.rs
+++ rustlings/exercises/enums/enums2.rs
@@ -1,11 +1,13 @@
 // enums2.rs
 // Execute `rustlings hint enums2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 #[derive(Debug)]
 enum Message {
-    // TODO: define the different variants used below
+    Move { x: u32, y: u32 },
+    Echo(String),
+    ChangeColor(u8, u8, u8),
+    Quit,
 }
 
 impl Message {
@@ -17,7 +19,7 @@ impl Message {
 fn main() {
     let messages = [
         Message::Move { x: 10, y: 30 },
-        Message::Echo(String::from("hello world")),
+        Message::Echo(String::from("hi mom")),
         Message::ChangeColor(200, 255, 255),
         Message::Quit,
     ];
--- rustlings/exercises/hashmaps/hashmaps3.rs
+++ rustlings/exercises/hashmaps/hashmaps3.rs
@@ -14,8 +14,6 @@
 
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 use std::collections::HashMap;
 
 // A structure to store team name and its goal details.
@@ -23,6 +21,25 @@ struct Team {
     name: String,
     goals_scored: u8,
     goals_conceded: u8,
+}
+
+fn update_team_score(
+    scores: &mut HashMap<String, Team>,
+    team_name: String,
+    goals_scored: u8,
+    goals_conceded: u8,
+) {
+    scores
+        .entry(team_name.clone())
+        .and_modify(|team| {
+            team.goals_scored += goals_scored;
+            team.goals_conceded += goals_conceded;
+        })
+        .or_insert(Team {
+            name: team_name,
+            goals_scored,
+            goals_conceded,
+        });
 }
 
 fn build_scores_table(results: String) -> HashMap<String, Team> {
@@ -40,6 +57,8 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         // will be the number of goals conceded from team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
+        update_team_score(&mut scores, team_1_name, team_1_score, team_2_score);
+        update_team_score(&mut scores, team_2_name, team_2_score, team_1_score);
     }
     scores
 }
